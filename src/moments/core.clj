2f505f8c45deab79ec67ac4e84a2843a115fc732
vaer-k <vincent@vaer-k.net>
2018-05-14 14:21:27 -0700

    Complete variance and std implementation

--- src/moments/core.clj
+++ src/moments/core.clj
@@ -2,21 +2,31 @@
 
 (def n (atom 0)) ; num records
 (def M (atom 0)) ; mean
-(def SS (atom 0)) ; sum of square
-(def sigma (atom 0)) ; standard deviation
+(def SS (atom 0)) ; sum of squares
 
 
-(defn inc-mean [x]
-  (+ @M (* (- x @M) (/ 1 @n))))
+(defn variance []
+  (/ @SS (- @n 1)))
 
-(defn inc-ss [x m]
-  (+ @SS (* (- x m) (- x @M))))
+(defn std []
+  (Math/sqrt (variance)))
+
+(defn inc-mean [x m n]
+  (-> (- x m)
+      (/ (inc n))
+      (+ m)))
+
+(defn inc-ss [x ss old-m new-m]
+  (-> (- x old-m)
+      (* (- x new-m))
+      (+ ss)))
 
 (defn update [x]
-  (let [m @M]
+  (let [old-m @M
+        new-m (inc-mean x old-m @n)]
     (reset! n (inc @n))
-    (reset! M (inc-mean x))
-    (reset! SS (inc-ss x m)))
+    (reset! M new-m)
+    (reset! SS (inc-ss x @SS old-m new-m)))
   nil)
 
 (defn clear []
